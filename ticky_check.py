ff23c639f88fb98239291745cdd35eb41968af87
kimito <kimito@example.com>
2020-09-26 13:02:50 +0900

    create dict to store the ranking of errors generated by the system

--- ticky_check.py
+++ ticky_check.py
@@ -6,11 +6,23 @@ import os
 
 path = os.getcwd()
 target_file=os.path.join(path, "syslog.log")
+
 error_msg = {}
 user_stat = {}
 
+user_name_pattern = r"\(([\w*.]*)\)"
+error_msg_pattern = r"[INFO|ERROR] ([\w ]*)"
+
+
 with open(target_file) as f:
     for line in f:
-        if "ERROR" not in line:
-            continue
-        print(line.strip())
+        result_usern = re.search(user_name_pattern, line)
+        if "ERROR" in line:
+            # error msg (error, count)
+            result_error = re.search(error_msg_pattern, line)
+            error_msg[result_error[1]] = error_msg.get(result_error[1], 0) + 1
+        # check user statistics (username, info, error)
+
+
+
+print(sorted(error_msg.items(), key=operator.itemgetter(1), reverse=True))
